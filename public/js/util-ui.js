d1f04f72b722d9fb60eee6644383309a58eb2dc8
Hayato Yokota <yokochie@gmail.com>
2019-07-11 22:10:46 +0900

    各入力項目のバリデーション処理を追加

--- public/js/util-ui.js
+++ public/js/util-ui.js
@@ -11,13 +11,16 @@ $(function () {
                 valueNotEquals: "msg"
             },
             family_name: {
-                required: true
+                required: true,
+                maxlength: 50
             },
             first_name: {
-                required: true
+                required: true,
+                maxlength: 50
             },
             people_num: {
-                required: true
+                required: true,
+                number: true
             }
         },
         messages: {
@@ -25,15 +28,17 @@ $(function () {
                 equalTo: "選択してください"
             },
             family_name: {
-                required: "必須項目です"
+                required: "必須項目です",
+                maxlength: "50文字以下で入力してください"
             },
             first_name: {
-                required: "必須項目です"
+                required: "必須項目です",
+                maxlength: "50文字以下で入力してください"
             },
             people_num: {
-                required: "必須項目です"
+                required: "必須項目です",
+                number: "数字のみ入力してください"
             }
-
         },
         errorClass: "error_msg",
         wrapper: "li"
