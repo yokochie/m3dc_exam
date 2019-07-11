0d6bf7552dc4d0c175bdb4fa18192b22c6b30742
Hayato Yokota <yokochie@gmail.com>
2019-07-12 00:41:49 +0900

    ログをDBに格納する処理を追加

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\LogStore;
+use App\ExamLog;
 
 class InputController extends Controller
 {
@@ -29,8 +31,21 @@ class InputController extends Controller
         $referer = $request->header('referer');
         $user_agent = $request->header('user-agent');
 
-        $log_store = new \App\LogStore();
+        $log_store = new LogStore;
         $log_store->store_request($input, $ip_address, $referer, $user_agent);
+
+        $exam_log = new ExamLog;
+        $exam_log->cmt_date = new \DateTime('now');
+        $exam_log->todohuken = $input['todohuken'];
+        $exam_log->fname = $input['first_name'];
+        $exam_log->lname = $input['family_name'];
+        $exam_log->viewcnt = $input['people_num'];
+        $exam_log->ip_addr = $ip_address;
+        $exam_log->referer = $referer;
+        $exam_log->usr_agent = $user_agent;
+
+        $exam_log->save();
+
         return redirect()->to('/viewpage');
     }
 }
