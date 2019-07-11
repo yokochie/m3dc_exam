9d70b851c8e92f266720405bc0673bb66bdd5ba3
Hayato Yokota <yokochie@gmail.com>
2019-07-11 22:39:23 +0900

    viewpage の内容を追加

--- resources/views/viewpages/viewpage.blade.php
+++ resources/views/viewpages/viewpage.blade.php
@@ -7,12 +7,15 @@
 
         	<div class="pageWrap">
                 <div class="panel panel-primary">
-                    <div class="panel-heading text-center"></div>
+                    <div class="panel-heading text-center">{{ $config['M3DC_SEMINAR_TITLE'] }} </div>
                     <div class="panel-body">
-                        <pre class="list-unstyled "></pre>
+                        <pre class="list-unstyled ">
+{{ $config['VIEW_INFO_DATE'] }}
+{{ $config['VIEW_INFO_TITLE'] }}
+{{ $config['VIEW_INFO_PROF'] }}</pre>
 
                         <div>
-                        	<!-- iframe コード入力 -->
+                            <iframe class="center-block" width="500" height="550" srcdoc="<img src=&quot;img/m3dc_logo.png&quot;>"></iframe>
                         </div>
 
                     </div>
@@ -22,7 +25,7 @@
                 </div>
 
                 <div class="col-sm-12 contactBox">
-                	<a target="_blank" href="">接続に関する技術的な質問等ございましたら、こちらからお問い合わせ下さい。</a>
+                	<a target="_blank" href="{{ $config['INQUIRY_URL'] }}">接続に関する技術的な質問等ございましたら、こちらからお問い合わせ下さい。</a>
             	</div>
 	        </div>
 
