8a74112403628567378b55f189ea23ecb6971cba
Hayato Yokota <yokochie@gmail.com>
2019-07-11 21:59:07 +0900

    各項目が空欄の時にエラーメッセージを表示するように変更

--- public/js/util-ui.js
+++ public/js/util-ui.js
@@ -10,12 +10,29 @@ $(function () {
             todohuken: {
                 valueNotEquals: "msg"
             },
-
+            family_name: {
+                required: true
+            },
+            first_name: {
+                required: true
+            },
+            people_num: {
+                required: true
+            }
         },
         messages: {
             todohuken: {
                 equalTo: "選択してください"
             },
+            family_name: {
+                required: "必須項目です"
+            },
+            first_name: {
+                required: "必須項目です"
+            },
+            people_num: {
+                required: "必須項目です"
+            }
 
         },
         errorClass: "error_msg",
